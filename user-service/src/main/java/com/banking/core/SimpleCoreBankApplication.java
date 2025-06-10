8e6725d26cb17f142bf039baafaf079383b7cf36
us <us@odds.team>
2025-06-11 01:42:01 +0700

    Rename SimpleCoreBankApplication

--- user-service/src/main/java/com/banking/core/SimpleCoreBankApplication.java
+++ user-service/src/main/java/com/banking/core/SimpleCoreBankApplication.java
@@ -1,13 +1,13 @@
-package com.banking.core.userservice;
+package com.banking.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class SimpleCoreBankApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(UserServiceApplication.class, args);
+    SpringApplication.run(SimpleCoreBankApplication.class, args);
   }
 
 }
