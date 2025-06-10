8ef1c5f4e28fe34b93b48a30404400f542fa43be
us <us@odds.team>
2025-06-10 08:42:11 +0700

    Add entity for mapping table 'user'

---
+++ user-service/src/main/java/com/banking/core/userservice/entity/User.java
@@ -0,0 +1,55 @@
+package com.banking.core.userservice.entity;
+
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"email"}),
+    @UniqueConstraint(columnNames = {"citizenId"})
+})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String email;
+
+  private String password; // hashed
+
+  @Column(length = 13, nullable = false)
+  private String citizenId;
+
+  private String thaiName;
+
+  private String englishName;
+
+  private String pinHash; // hashed 6-digit PIN
+
+  private String role;
+
+//  @ElementCollection(fetch = FetchType.EAGER)
+//  @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+//  @Column(name = "role")
+//  private List<String> roles;
+}
