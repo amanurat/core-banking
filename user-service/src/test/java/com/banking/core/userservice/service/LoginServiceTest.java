104d1a98764f974828a525919e72e0688913ad90
us <us@odds.team>
2025-06-10 08:45:01 +0700

    Add LoginService

---
+++ user-service/src/test/java/com/banking/core/userservice/service/LoginServiceTest.java
@@ -0,0 +1,15 @@
+package com.banking.core.userservice.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginServiceTest {
+
+  //unit test for login service
+
+  //validate correct PIN for user then return true or 204 No Content
+
+  //validate incorrect PIN then throw InvalidPinException
+
+  //PIN hashing works (this mean stored hash <> input PIN
+
+}
