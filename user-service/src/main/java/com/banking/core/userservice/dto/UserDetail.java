d23e57ae65847ab43f4445c12b7418964298e6aa
us <us@odds.team>
2025-06-11 18:37:29 +0700

    feat: add UserDetail class for security context management

---
+++ user-service/src/main/java/com/banking/core/userservice/dto/UserDetail.java
@@ -0,0 +1,18 @@
+package com.banking.core.userservice.dto;
+
+import com.banking.core.userservice.entity.User;
+import java.util.Collection;
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+@Data
+public class UserDetail {
+  private String username; // mapping email
+    private String password;
+    private String citizenId;
+    private String thaiName;
+    private String englishName;
+    private String role; // CUSTOMER, TELLER
+}
